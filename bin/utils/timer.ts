db5906eaa3f7f3a72fefac774ef662e098ff4a2d
mohebbi <mohebbisadegh@gmail.com>
2022-04-12 12:04:36 +0430

    fix timer util undefined var

--- bin/utils/timer.ts
+++ bin/utils/timer.ts
@@ -3,11 +3,11 @@ const timer = () => {
   let _endTime: number;
 
   const _start = () => {
-    _startTime = performance.now();
+    _startTime = Date.now();
   };
 
   const _end = () => {
-    _endTime = performance.now();
+    _endTime = Date.now();
     const timeDiff = (_endTime - _startTime) / 1000;
 
     return Math.round(timeDiff);
